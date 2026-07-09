5d2d1b9c026fbd5560a39917daad212ef768163c
LHMezentier <17418306725@cefet-rj.br>
2026-07-09 15:47:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ trq.onreadystatechange = ()=>{
 }
 }
 const capitalizeFirstLetter = (str) => {
-  if (!str) return ""; // Handle empty strings
+  if (!str) return "";
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 const resetaroutput = () =>
